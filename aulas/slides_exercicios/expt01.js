12d24151ebd29d5d3a11ab8b77098506f59341bb
Kayque Estevão <kayque2911@gmail.com>
2025-08-24 21:50:53 -0300

    feat: anexando slide e resolvendo exercicios

---
+++ aulas/slides_exercicios/expt01.js
@@ -0,0 +1,14 @@
+/* exercicios com funções
+//ex01
+function saudar(nome){
+    return `Olá ${nome}`
+}
+
+console.log(saudar("Kayque"));
+
+//ex02
+function dobrar(numero){
+    return numero * 2
+}
+console.log(`Número dobrado: ${dobrar(6)}`);
+*/
